e24fce87168927f4e38bbb9da7ea0ae6f864bc85
thedoodler <ramprakashsmail@gmail.com>
2020-12-12 14:49:37 +0530

    ADD:  added cpp template

---
+++ cpp_problem/template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    vector<int> lst;
+    cin >> n; 
+    for (i=0; i<n; i++){
+        cin >> val;
+        lst.push_back(val);
+  }
+}
